df14ee6210b0ba706851cb812a1909eb2138410d
ss92426 <69694832+ss92426@users.noreply.github.com>
2020-08-14 13:01:52 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Sarvesh Shanmugam");
+		System.out.println("The video game I play the most is leauge of legends");
+		System.out.println("I plan on majoring in engineering");
+		System.out.println("My favorite food is sushi");
+	}
+}
